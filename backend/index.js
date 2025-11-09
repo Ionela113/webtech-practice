ee0883c539ef85ade9f5d8fe4bfa29dfa28b32bd
Ionela113 <popaionela023@gmail.com>
2025-11-09 20:48:52 +0200

    Added first API with button

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
 import express from "express"
+import cors from "cors"
+
+
 const app = express()
 const PORT = 3001;
-app.use(express.json());
+app.use(express.json()); // middleware
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+
 
 app.get("/", (req, res) => {
     res.send("Hello world.");
--- frontend/src/pages/homePage/index.jsx
+++ frontend/src/pages/homePage/index.jsx
@@ -1,11 +1,23 @@
 import "./index.css"
+import axios from "axios";
 
+// function async onButtonClick() {
 
+// }
+
+const onButtonClick = async () => {
+    try {
+        const response = await axios.get("http://localhost:3001/");
+        console.log(response.data);
+    } catch (err) {
+        console.log("Nu a functionat")
+    }
+}
 
 export const HomePage = () => {
     return (
         <div>
-            <button>TEST</button>
+            <button onClick={onButtonClick}>TEST</button>
         </div>
     )
 }
